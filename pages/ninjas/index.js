58754538e1873187b5b53312cc33e16ab7f46bc0
Link1986 <ali.ghoudi@gmail.com>
2021-12-25 22:39:55 +0100

    updated index.js of ninjas file

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -16,7 +16,7 @@ const Ninjas = ({ ninjas }) => {
     <div>
       <h1>All Ninjas</h1>
       {ninjas.map(ninja => (
-        <Link key={ninja.id}>
+        <Link href={`/ninjas/${ninja.id}`} key={ninja.id}>
           <a className={styles.single}>
             <h3>{ ninja.name }</h3>
           </a>
