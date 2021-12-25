076ea89e3e3f39b8a5ecf376dd98411999a8534b
Link1986 <ali.ghoudi@gmail.com>
2021-12-25 22:34:47 +0100

    added [id].js

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim amet fuga ea sunt doloremque dolorem labore dolore voluptate beatae. Vitae cum reiciendis sunt atque quidem maxime nihil, odio maiores odit.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi obcaecati nihil quaerat aspernatur error enim harum similique molestiae assumenda iusto ratione dignissimos, dolorem ab doloribus vero quo nisi, necessitatibus voluptates?</p>
+      </div>
+    </>    
+  );
+}
+ 
+export default About;
