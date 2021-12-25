6cfb22cfc2a4e9ec437f5ff3e99a2128e95dcdc2
Link1986 <ali.ghoudi@gmail.com>
2021-12-25 23:24:05 +0100

    updated id.js of ninjas file

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -2,10 +2,11 @@ export const getStaticPaths = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
   const data = await res.json();
 
+  // map data to an array of path objects with params (id)
   const paths = data.map(ninja => {
     return {
-      paths: [{}, {}, { params: { id: ninja.id.toString() } }]
-    }  
+      params: { id: ninja.id.toString() }
+    }
   })
 
   return {
@@ -14,12 +15,25 @@ export const getStaticPaths = async () => {
   }
 }
 
-const Details = () => {
-  return ( 
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id);
+  const data = await res.json();
+
+  return {
+    props: { ninja: data }
+  }
+}
+
+const Details = ({ ninja }) => {
+  return (
     <div>
-      <h1>Details Page</h1>
+      <h1>{ ninja.name }</h1>
+      <p>{ ninja.email }</p>
+      <p>{ ninja.website }</p>
+      <p>{ ninja.address.city }</p>
     </div>
   );
 }
- 
+
 export default Details;
